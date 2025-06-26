e0a7670a6b514e579e516701579bf470a45a7096
Pablo e lucas <geovanaassis073@gmail.com>
2025-06-26 14:52:14 -0300

    26/06

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="content">
+        <h1 className="title">CRUD Database</h1>
+        <table className="tableCRUD">
+          <thead className="theadCRUD">
+            <tr className="trTheadCRUD">
+              <th className="thCRUD">ID</th>
+              <th className="thCRUD">Nome</th>
+              <th className="thCRUD">Sobrenome</th>
+              <th className="thCRUD">E-mail</th>
+              <th className="thCRUD">Telefone</th>
+              <th className="thCRUD">Endereço</th>
+              <th className="thCRUD">Emprego</th>
+              <th className="thCRUD">Ação</th>
+            </tr>
+          </thead>
+          <tbody className="TbodyCRUD">
+            <tr className="trTbodyCRUD">
+              <td className="tdCRUD">01</td>
+              <td className="tdCRUD">Geovana</td>
+              <td className="tdCRUD">Assis</td>
+              <td className="tdCRUD">geovanaassis073@gmail.com</td>
+              <td className="tdCRUD">(33) 99937-9387</td>
+              <td className="tdCRUD">São Felix de Minas - MG</td>
+              <td className="tdCRUD">Aluna</td>
+              <td className="tdCRUD">
+                <div className="ContentButtons">
+                  <button className="buttonEdit">Editar</button>
+                  <button className="buttonDel">Excluir</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <button className="buttonAdd">Adicionar novo item</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
